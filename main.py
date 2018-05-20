80e421482479ef4fdc266959fd4b31e71b156df8
kazuma murata <mrt_014_kzm@yahoo.co.jp>
2018-05-20 17:46:12 +0900

    main関数修正

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
+import sys
+
 from modules.action.follow import FollowAction
 
 if __name__ == "__main__":
-    FollowAction.follow_from_tweet_search("テスト")
+
+    functions = {
+        "profile_follow" : {
+            "description": "プロフィールを検索してフォロー"
+        },
+        "tweet_follow" : {
+            "description": "ツイートを検索してフォロー"
+        }
+    }
+
+    try:
+        print("==============================================")
+        print("exec....")
+
+        args = sys.argv
+        if len(args) == 1:
+            # 引数がなければ警告
+            print("第2引数を指定してください")
+            for key in functions.keys():
+                print(key + ": " + functions[key]["description"])
+            raise Exception
+
+        if args[1] not in functions:
+            # 第1引数に一致する関数がなければエラー
+            print("引数が正しくありません")
+            raise Exception
+
+        # 引数に一致する関数を探して実行
+        if args[1] == "profile_follow":
+            FollowAction.follow_from_prof_search("相互フォロー")
+        elif args[1] == "tweet_follow":
+            FollowAction.follow_from_tweet_search("テスト")
+
+    finally:
+        print("==============================================")
